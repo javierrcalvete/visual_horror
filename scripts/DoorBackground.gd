64486f74b6a73879935881f6646e3b1c821ad784
javi <javierrcalvete@gmail.com>
2024-06-30 17:32:23 -0300

    added dialogue/function to scene level_1 door fade in

--- scripts/DoorBackground.gd
+++ scripts/DoorBackground.gd
@@ -4,3 +4,10 @@ extends Sprite2D
 
 func _ready() -> void:
 	$AnimationPlayer.play("fade_in")
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	var animation_finishes=anim_name
+	print(animation_finishes)
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/main.dialogue"), "start")
