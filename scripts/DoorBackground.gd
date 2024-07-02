3686f3f2c5407e3cea471a7a950d3e64dac818be
javi <javierrcalvete@gmail.com>
2024-07-02 12:28:02 -0300

    added collision, area, control nodes to door, change mouse filter to pass

--- scripts/DoorBackground.gd
+++ scripts/DoorBackground.gd
@@ -11,3 +11,11 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	var animation_finishes=anim_name
 
 	DialogueManager.show_dialogue_balloon(load("res://dialogue/main.dialogue"), "start")
+
+
+func _on_area_2d_mouse_entered() -> void:
+	print("mouse enter")
+
+
+func _on_area_2d_mouse_exited() -> void:
+	print("mouse exit")
