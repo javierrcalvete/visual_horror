fc55292fbfa3e93db2a7152e299b18de448bd97b
javi <javierrcalvete@gmail.com>
2024-07-02 13:15:48 -0300

    added functionality to change to scene2 by clicking the MouseSignalDoor

--- scripts/DoorBackground.gd
+++ scripts/DoorBackground.gd
@@ -15,9 +15,16 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 
 
 
-func _on_control_mouse_entered() -> void:
-	print("mouse enter")
+func _on_control_mouse_entered():
+	pass
+	#get_tree().change_scene_to_file("res://scenes/level_2.tscn")
 
 
 func _on_control_mouse_exited() -> void:
 	print("mouse exit")
+
+# func for clicking the door and changin scenes,
+func _on_mouse_signal_door_gui_input(event) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+		
+			get_tree().change_scene_to_file("res://scenes/level_2.tscn")
