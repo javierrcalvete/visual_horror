92c0c480287fe7fbf08e6c26cc2278620b441907
javi <javierrcalvete@gmail.com>
2024-07-02 23:20:30 -0300

    added line for finding a knife and created lelve2Manager

--- scripts/Hallway.gd
+++ scripts/Hallway.gd
@@ -13,10 +13,11 @@ func _on_hallway_animation_player_animation_finished(anim_name: StringName) -> v
 	DialogueManager.show_dialogue_balloon(load("res://dialogue/main.dialogue"), "level_2")
 
 
-
+# func for clicking in the blood and finding the knife
 func _on_mouse_signal_knife_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
 		if !Inventory.inventory_dictionary.has("Knife"):
 			Inventory.inventory_dictionary["Knife"] = knife_item
+			DialogueManager.show_dialogue_balloon(load("res://dialogue/main.dialogue"), "knife_found")
 			knife_item.visible = true
 			print(Inventory.inventory_dictionary)
