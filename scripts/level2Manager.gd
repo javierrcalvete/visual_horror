06858a2f57176af96db18821011972051e22033e
javi <javierrcalvete@gmail.com>
2024-07-07 18:51:35 -0300

    added player script  and added player health when enemy enters

--- scripts/level2Manager.gd
+++ scripts/level2Manager.gd
@@ -5,6 +5,7 @@ extends Node
 @onready var inventory: Control = get_node("../Level2/Inventory")
 @onready var knife_item: Node2D = get_node("../Level2/KnifeItem")
 @onready var enemy: Node2D
+@onready var player_label_health: Label = get_node("../Level2/PlayerHealth")
 
 var animation_player: AnimationPlayer
 var knife_was_found :bool = false
@@ -20,6 +21,7 @@ func knife_found():
 
 #call for animation of the enemy and shows the inventory
 func enemy_1_enter():
+	player_label_health.text = str(Player.player_health)
 	inventory.show_item_slot(0)
 	enemy = get_node("../Level2/Enemy_1")
 	enemy.show()
@@ -29,11 +31,13 @@ func enemy_1_enter():
 
 func battle_enemy_1():
 	#print(str(inventory.damage_item_selected))
+		
+		if enemy.enemy_1_health > 2 and enemy.enemy_1_health <=10:
+			enemy.enemy_1_health -= inventory.damage_item_selected
+			#print(enemy.enemy_1_health)
+			
+			DialogueManager.show_dialogue_balloon(load("res://dialogue/main.dialogue"), "enemy_1_damage")
+		else:
+			animation_player.play("fade_out")
 	
-	if enemy.enemy_1_health > 2 and enemy.enemy_1_health <=10:
-		enemy.enemy_1_health -= inventory.damage_item_selected
-		print(enemy.enemy_1_health)
-		player_action = true
-	else:
-		animation_player.play("fade_out")
 				
